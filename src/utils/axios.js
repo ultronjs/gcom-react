78923d3c95d39d7ec180c5934d74cf5927bb6b2d
ultron07 <ultronreactjsdev@gmail.com>
2022-04-01 10:52:28 +0530

    [update]:removing interceptors
    removing interceptors .Not required anymore.
    git Comment Resolve

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,24 +5,8 @@ export const publicInstance = axios.create({
   headers: {'Accept': '*/*'}
 });
 
-publicInstance.interceptors.request.use(async req => {
-  console.log("intercerper",req)
-  return req
-})
-publicInstance.interceptors.response.use(async (res) => {
-  console.log("intercerper response", res);
-  return res;
-});
 export const privateInstance = axios.create({
   baseURL: "/api/",
   timeout: 1000,
   headers: { Accept: "*/*", authorization: localStorage.getItem("token") },
-});
-privateInstance.interceptors.request.use(async (req) => {
-  console.log("intercerper", req);
-  return req;
-});
-privateInstance.interceptors.response.use(async (res) => {
-  console.log("intercerper response", res);
-  return res;
 });
